5559e459a6a4e80f627ba09c65535db2e22d73e1
Alexandr Blinov <alexandr_b_2000@mail.ru>
2024-05-06 23:36:49 +0300

    Difficult tasks

--- src/App.js
+++ src/App.js
@@ -2,40 +2,50 @@ import styles from './App.module.css';
 import { useState } from 'react';
 
 export const App = () => {
-	const NUMS = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
+	const NUMS = ['C', '-', '+', '=', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
 
 	const [operand1, setOperand1] = useState('');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
 	const [result, setResult] = useState('');
 
-	const getResult = () => {
-		let res;
-		if (operator === '+') {
-			res = Number(operand1) + Number(operand2);
-		} else if (operator === '-') {
-			res = Number(operand1) - Number(operand2);
-		}
-
-		setOperand2('');
-		setOperator('');
-		setResult(res);
-	};
-
 	const handleOperatorClick = (newOperator) => {
 		if (result !== '') {
 			setOperand1(String(result));
 			setResult('');
 		}
-		setOperator(newOperator);
+		if (newOperator === '+' && operand1) {
+			setOperator(newOperator);
+		}
+		if (newOperator === '-' && operand1) {
+			setOperator(newOperator);
+		}
+		if (newOperator === 'C') {
+			setOperand1('');
+			setOperand2('');
+			setOperator('');
+			setResult('');
+		}
+		if (newOperator === '=' && operand1 && operand2) {
+			let res;
+			if (operator === '+') {
+				res = Number(operand1) + Number(operand2);
+			} else if (operator === '-') {
+				res = Number(operand1) - Number(operand2);
+			}
+
+			setOperand2('');
+			setOperator('');
+			setResult(res);
+		}
 	};
 
 	const handleNumberClick = (event) => {
 		if (operator === '' && operand1.length < 5) {
-			setOperand1(`${operand1}${event.target.textContent}`);
+			setOperand1(operand1 + event.target.textContent);
 		}
 		if (operand1 !== '' && operator !== '' && operand2.length < 5) {
-			setOperand2(`${operand2}${event.target.textContent}`);
+			setOperand2(operand2 + event.target.textContent);
 		}
 	};
 
@@ -57,49 +67,19 @@ export const App = () => {
 				)}
 			</div>
 			<div className="buttons">
-				<button
-					className={styles['btn-calc']}
-					onClick={() => {
-						setOperand1('');
-						setOperand2('');
-						setOperator('');
-						setResult('');
-					}}
-				>
-					C
-				</button>
-				<button
-					className={styles['btn-calc']}
-					onClick={() => {
-						operand1 !== '' ? handleOperatorClick('+') : setOperator('');
-					}}
-				>
-					+
-				</button>
-				<button
-					className={styles['btn-calc']}
-					onClick={() => {
-						operand1 ? handleOperatorClick('-') : setOperator('');
-					}}
-				>
-					-
-				</button>
-				<button
-					className={styles['btn-calc']}
-					onClick={() => {
-						if (operand1 !== '' && operator !== '' && operand2 !== '') {
-							getResult();
-						}
-					}}
-				>
-					=
-				</button>
-
 				{NUMS.map((num) => (
 					<button
-						className={styles['btn-num']}
+						className={
+							['C', '+', '-', '='].includes(num)
+								? styles['btn-calc']
+								: styles['btn-num']
+						}
 						key={num}
-						onClick={(event) => handleNumberClick(event)}
+						onClick={(event) => {
+							!isNaN(Number(event.target.textContent))
+								? handleNumberClick(event)
+								: handleOperatorClick(event.target.textContent);
+						}}
 					>
 						{num}
 					</button>
