5f403c7e9cdc02ee0c769448a6a6acff06f132a0
Alexandr Blinov <alexandr_b_2000@mail.ru>
2024-05-06 23:24:24 +0300

    completed task 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const NUMS = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
 
-export default App;
+	const [operand1, setOperand1] = useState('');
+	const [operand2, setOperand2] = useState('');
+	const [operator, setOperator] = useState('');
+	const [result, setResult] = useState('');
+
+	const getResult = () => {
+		let res;
+		if (operator === '+') {
+			res = Number(operand1) + Number(operand2);
+		} else if (operator === '-') {
+			res = Number(operand1) - Number(operand2);
+		}
+
+		setOperand2('');
+		setOperator('');
+		setResult(res);
+	};
+
+	const handleOperatorClick = (newOperator) => {
+		if (result !== '') {
+			setOperand1(String(result));
+			setResult('');
+		}
+		setOperator(newOperator);
+	};
+
+	const handleNumberClick = (event) => {
+		if (operator === '' && operand1.length < 5) {
+			setOperand1(`${operand1}${event.target.textContent}`);
+		}
+		if (operand1 !== '' && operator !== '' && operand2.length < 5) {
+			setOperand2(`${operand2}${event.target.textContent}`);
+		}
+	};
+
+	return (
+		<div className={styles['calc']}>
+			<div className={styles['calc-screen']}>
+				{result === '' ? (
+					<p
+						className={
+							operand1.length + operand2.length > 9
+								? styles['text-formatting']
+								: null
+						}
+					>
+						{operand1 + operator + operand2}
+					</p>
+				) : (
+					<p className={styles['active']}>{result}</p>
+				)}
+			</div>
+			<div className="buttons">
+				<button
+					className={styles['btn-calc']}
+					onClick={() => {
+						setOperand1('');
+						setOperand2('');
+						setOperator('');
+						setResult('');
+					}}
+				>
+					C
+				</button>
+				<button
+					className={styles['btn-calc']}
+					onClick={() => {
+						operand1 !== '' ? handleOperatorClick('+') : setOperator('');
+					}}
+				>
+					+
+				</button>
+				<button
+					className={styles['btn-calc']}
+					onClick={() => {
+						operand1 ? handleOperatorClick('-') : setOperator('');
+					}}
+				>
+					-
+				</button>
+				<button
+					className={styles['btn-calc']}
+					onClick={() => {
+						if (operand1 !== '' && operator !== '' && operand2 !== '') {
+							getResult();
+						}
+					}}
+				>
+					=
+				</button>
+
+				{NUMS.map((num) => (
+					<button
+						className={styles['btn-num']}
+						key={num}
+						onClick={(event) => handleNumberClick(event)}
+					>
+						{num}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+};
